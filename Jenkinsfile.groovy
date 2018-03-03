b0ea2789391da71fdc42a320b80f9026d0e4714f
Rob <brem_robert@hotmail.com>
2018-03-03 21:22:51 +0100

    [BUG] add container templates

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,4 +1,12 @@
-podTemplate(label: 'mypod') {
+podTemplate(label: 'mypod', containers: [
+        containerTemplate(name: 'docker', image: 'docker', ttyEnabled: true, command: 'cat'),
+        containerTemplate(name: 'kubectl', image: 'lachlanevenson/k8s-kubectl:v1.8.0', command: 'cat', ttyEnabled: true),
+        containerTemplate(name: 'curl', image: 'khinkali/jenkinstemplate:0.0.3', command: 'cat', ttyEnabled: true),
+        containerTemplate(name: 'maven', image: 'maven:3.5.2-jdk-8', command: 'cat', ttyEnabled: true)
+],
+        volumes: [
+                hostPathVolume(mountPath: '/var/run/docker.sock', hostPath: '/var/run/docker.sock'),
+        ]) {
     node('mypod') {
         properties([
                 buildDiscarder(
