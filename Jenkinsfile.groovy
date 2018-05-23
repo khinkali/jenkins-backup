92fc82a534d763aef001725016528d1767e3e4fe
Rob <brem_robert@hotmail.com>
2018-05-23 11:23:12 +0200

    [REFAC] use library

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -21,24 +21,11 @@ podTemplate(label: 'mypod', containers: [
 
         stage('create backup') {
             def kc = 'kubectl'
+            def containerPath = '/var/jenkins_home'
+            def containerName = 'jenkins'
+            def podLabel = 'app=jenkins'
             container('kubectl') {
-                def jenkinsPods = sh(
-                        script: "${kc} get po -l app=jenkins --no-headers",
-                        returnStdout: true
-                ).trim()
-                def podNameLine = jenkinsPods.split('\n')[0]
-                def startIndex = podNameLine.indexOf(' ')
-                if (startIndex == -1) {
-                    return
-                }
-                def podName = podNameLine.substring(0, startIndex)
-                sh "${kc} exec ${podName} -- git -C '/var/jenkins_home' config user.email \"jenkins@khinkali.ch\""
-                sh "${kc} exec ${podName} -- git -C '/var/jenkins_home' config user.name \"Jenkins\""
-                sh "${kc} exec ${podName} -- git -C '/var/jenkins_home' add --all"
-                sh "${kc} exec ${podName} -- git -C '/var/jenkins_home' diff --quiet && ${kc} exec ${podName} -- git -C '/var/jenkins_home' diff --staged --quiet || ${kc} exec ${podName} -- git -C '/var/jenkins_home' commit -am 'new_version'"
-                withCredentials([usernamePassword(credentialsId: 'bitbucket', passwordVariable: 'GIT_PASSWORD', usernameVariable: 'GIT_USERNAME')]) {
-                    sh "${kc} exec ${podName} -- git -C '/var/jenkins_home' push https://${GIT_USERNAME}:${GIT_PASSWORD}@bitbucket.org/khinkali/jenkins_backup"
-                }
+                backup(podLabel, containerName, containerPath, kc)
             }
         }
 
